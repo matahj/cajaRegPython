4685ca507a22dd8c9d3fbd296fe2f4fd3f956574
matahj <matahj@gmail.com>
2020-05-19 21:16:24 -0500

    Genera compra terminada

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 
 import CajaReg as cr
 
-print("Iniciando caja...")
+
 c1 = cr.Caja()
 print("Estado de la caja: " + c1.getEstado() )
 
-print("Iniciando Compra...")
-c1.comprar()
-print("Fin de la compra....")
+while c1.getEstado() == 'on' :
+    
+    c1.imprimeMenu()
+    print("Seleccione opción: ")
+    resp = int(input())
+
+    if resp== 1:
+        c1.comprar()
+    elif resp == 2:
+        c1.imprimeNumeroTickets()
+    elif resp == 3:
+        pass
+    elif resp == 4:
+        c1.corteCaja()
+    elif resp == 5:
+        c1.apagar()
+    else:
+        print("Opción no válida")
+
+    
+
+#    print("Nueva Compra (si/no): ")
+#    resp = input()
+#    if(resp=='no'):
+#        c1.apagar()
+#    else:
+#        pass
+
+#print("Estado de la caja: " + c1.getEstado() )
 
 
